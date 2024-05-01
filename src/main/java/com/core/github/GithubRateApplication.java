006ff55687c6f5e2cca9e1679dc83b356bc4c847
Geon.C <chlrjs1324@gmail.com>
2024-05-01 17:05:19 +0900

    Initial commit

---
+++ src/main/java/com/core/github/GithubRateApplication.java
@@ -0,0 +1,13 @@
+package com.core.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRateApplication.class, args);
+    }
+
+}
